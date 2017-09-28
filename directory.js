2d94907936fa3e0da69319807371b7967d5e0afc
Leonardo Lazbal <llazbal@gmail.com>
2017-09-28 00:50:56 -0300

    Defining Your Controller In UI Router

--- directory.js
+++ directory.js
@@ -6,11 +6,12 @@ angular.module('directoryApp', ['ngAnimate', 'ui.router'])
         $stateProvider
             .state('home', {
                 url: '/',
-                templateUrl: 'home.html'
+                templateUrl: '/static/home.html',
+                controller: 'directoryController as dirList'
             })        
             .state('about', {
                 url: '/about',
-                templateUrl: 'about.html'
+                templateUrl: '/static/about.html'
             });
     })
     .controller('directoryController', function(){
